e2a5a61858944326a2f580e2a25ac5768bfba7b8
Alejandro Reyes <127257358+alereyes8714@users.noreply.github.com>
2024-04-09 12:28:48 -0300

    Add files via upload
    trabajo alkemy biblioteca

---
+++ biblioteca/src/main/java/ui/Usuario.java
@@ -0,0 +1,52 @@
+package ui;
+
+import java.util.ArrayList;
+
+
+public class Usuario {
+    private String nombre;
+    private String id;
+    private ArrayList<Libro> librosPrestados;
+
+    //CONSTRUCTORES
+
+
+    public Usuario(String nombre, String id) {
+        this.nombre = nombre;
+        this.id = id;
+        this.librosPrestados = new ArrayList<>();
+    }
+
+    public Usuario() {
+    }
+
+    //GETTERS AND SETTERS
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public ArrayList<Libro> getLibrosPrestados() {
+        return librosPrestados;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+    //TO STRING
+
+    @Override
+    public String toString() {
+        return "Usuario:" +
+                "Nombre:" + nombre +
+                ", Id: " + id +'.';
+    }
+}
